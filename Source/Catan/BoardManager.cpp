097da757c7dd64206ba9d86239f30688b603fdb7
Jesse Olmer <jesse@wickedgoodtimes.com>
2016-08-08 00:20:07 -0700

    Initial commit

---
+++ Source/Catan/BoardManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Catan.h"
+#include "BoardManager.h"
+
+BoardManager::BoardManager()
+{
+}
+
+BoardManager::~BoardManager()
+{
+}
---
+++ Source/Catan/BoardManager.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class CATAN_API BoardManager
+{
+public:
+	BoardManager();
+	~BoardManager();
+};
